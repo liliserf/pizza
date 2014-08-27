ebb30260abc04672aedd699e4e8bef5c066fa38a
Lili Serfaty <lili.serfaty@gmail.com>
2014-08-27 17:18:52 -0500

    add Pizza.initialize toppings array and change 'vegetarian: false' to 'vegetarian=false'

--- pizza.rb
+++ pizza.rb
@@ -1,5 +1,19 @@
 class Pizza
+  
+  attr_accessor :toppings
+
+  def initialize(toppings=[Topping.new('cheese', vegetarian: true)])
+    @toppings = toppings
+  end
 end
 
 class Topping
+  
+  attr_accessor :name, :vegetarian
+
+  def initialize(name, vegetarian: false)
+    @name = name
+    @vegetarian = vegetarian 
+  end
+
 end
